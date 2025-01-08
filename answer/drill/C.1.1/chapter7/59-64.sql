285f0c80920f32442e2f22dc2f67ba09bc55f0a7
krs1 <salvare000919@gmail.com>
2025-01-09 00:16:27 +0900

    fix: sql

--- answer/drill/C.1.1/chapter7/59-64.sql
+++ answer/drill/C.1.1/chapter7/59-64.sql
@@ -21,15 +21,21 @@ SELECT A.日付, (SELECT MAX(入金額) FROM 取引 WHERE 口座番号 ='3104451
 FROM (SELECT 日付 FROM 取引 WHERE 口座番号 = '3104451' GROUP BY 日付 HAVING SUM(入金額) > 0 AND SUM(出金額) > 0) AS A
 
 -- 自分作
-SELECT * 
+-- 日付と最大額は条件が違うので別々に抽出してあげないといけない
+SELECT 抽出後日付.日付, 最大額.最大入金額, 最大額.最大出金額
 FROM (
-    SELECT 日付, SUM(入金額) AS 最大入金額, SUM(出金額) AS 最大出金額
+    SELECT '1' AS id, 日付, SUM(入金額) AS 合計入金額, SUM(出金額) AS 合計出金額
     FROM 取引
     WHERE 口座番号 = '3104451'
     GROUP BY 日付
-    HAVING NOT ISNULL(最大入金額) AND NOT ISNULL(最大出金額)
-) AS a
-
+    HAVING NOT ISNULL(合計入金額) AND NOT ISNULL(合計出金額)
+) AS 抽出後日付
+JOIN (
+	SELECT '1' AS id, MAX(入金額) AS 最大入金額, MAX(出金額) AS 最大出金額
+	FROM 取引
+	WHERE 口座番号 = '3104451'
+) AS 最大額
+ON 抽出後日付.id = 最大額.id
 
 -- 64
 INSERT INTO 廃止口座 SELECT * FROM 口座 WHERE 口座番号 = '2761055';
