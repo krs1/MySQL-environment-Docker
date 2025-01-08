48cd4526b5cddc4aa2bb0c409b5eb789ff2f5d7c
krs1 <salvare000919@gmail.com>
2025-01-08 17:51:50 +0900

    fix sql

--- answer/drill/C.1.1/chapter7/59-64.sql
+++ answer/drill/C.1.1/chapter7/59-64.sql
@@ -4,7 +4,8 @@ SET 残高 = 残高 + (SELECT COALESCE(SUM( 入金額 ), 0) - COALESCE (SUM(出�
 更新日 = '2024-01-11' WHERE 口座番号 = '0351333'
 
 -- 60
-SELECT 残高, (SELECT SUM(入金額) FROM 取引 WHERE 口座番号 = '1115600' AND 日付 = '2023-12-28') AS 入金額合計,
+SELECT 残高,
+(SELECT SUM(入金額) FROM 取引 WHERE 口座番号 = '1115600' AND 日付 = '2023-12-28') AS 入金額合計,
 (SELECT SUM(出金額) FROM 取引 WHERE 口座番号 = '1115600' AND 日付 = '2023-12-28') AS 出金額合計
 FROM 口座 WHERE 口座番号 = '1115600'
 
@@ -12,12 +13,23 @@ FROM 口座 WHERE 口座番号 = '1115600'
 SELECT 口座番号 , 名義 , 残高 FROM 口座 WHERE 口座番号 IN (SELECT 口座番号 FROM 取引 WHERE 入金額 >= 1000000)
 
 -- 62
-SELECT * FROM 口座 WHERE 更新日 >ALL (SELECT 日付 FROM 取引 )
+SELECT * FROM 口座 WHERE 更新日 > ALL (SELECT 日付 FROM 取引 )
 
 -- 63
 SELECT A.日付, (SELECT MAX(入金額) FROM 取引 WHERE 口座番号 ='3104451') AS 最大入金額, 
 (SELECT MAX(出金額) FROM 取引 WHERE 口座番号 = '3104451') AS 最大出金額
 FROM (SELECT 日付 FROM 取引 WHERE 口座番号 = '3104451' GROUP BY 日付 HAVING SUM(入金額) > 0 AND SUM(出金額) > 0) AS A
+
+-- 自分作
+SELECT * 
+FROM (
+    SELECT 日付, SUM(入金額) AS 最大入金額, SUM(出金額) AS 最大出金額
+    FROM 取引
+    WHERE 口座番号 = '3104451'
+    GROUP BY 日付
+    HAVING NOT ISNULL(最大入金額) AND NOT ISNULL(最大出金額)
+) AS a
+
 
 -- 64
 INSERT INTO 廃止口座 SELECT * FROM 口座 WHERE 口座番号 = '2761055';
