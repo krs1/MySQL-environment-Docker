f74732b4433a50dc8495d4f21144c23ea860fc49
krs1 <salvare000919@gmail.com>
2025-01-20 17:05:38 +0900

    fix: sql

--- answer/drill/C.1.2/chapter4/25-33.sql
+++ answer/drill/C.1.2/chapter4/25-33.sql
@@ -16,6 +16,7 @@ SELECT * FROM 商品 ORDER BY 単価 , 商品区分 , 商品コード
 LIMIT 5, 15
 
 -- 30
+-- カラム6番目は売上個数
 SELECT * FROM 廃番商品 WHERE 廃番日 >= '2022-12-01' AND 廃番日 < '2023-01-01' 
 UNION
 SELECT * FROM 廃番商品 WHERE 売上個数 > 100
